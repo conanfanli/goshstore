b897837c6af36f1a35e4eba45841999f914d4946
Conan Li <conanlics@gmail.com>
2015-11-23 00:12:31 -0400

    : add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='goshstore',
+    version='0.1',
+    description='Django get-or-set hstore field',
+    url='https://github.com/conanfanli/goshstore',
+    packages=find_packages(),
+    install_requires=['django>=1.8,<1.9'],
+    classifiers=[
+        'Programming Language :: Python :: 3.4'
+    ]
+)

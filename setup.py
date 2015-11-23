703bd99b3d2143eee175a19c6f93e84e8d4ce990
Conan Li <conanlics@gmail.com>
2015-11-23 00:31:31 -0400

    : more setup

--- setup.py
+++ setup.py
@@ -5,12 +5,20 @@ from setuptools import setup, find_packages
 
 setup(
     name='goshstore',
-    version='0.1',
+    version='0.1.0dev1',
     description='Django get-or-set hstore field',
     url='https://github.com/conanfanli/goshstore',
-    packages=find_packages(),
+    packages=find_packages(exclude=['doc', 'tests*']),
     install_requires=['django>=1.8,<1.8.9'],
     classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'License :: OSI Approved :: MIT License',
+
         'Programming Language :: Python :: 3.4'
-    ]
+    ],
+    keywords='django hstore postgres',
+    author='Conan Fan Li',
+    author_email='conanlics@gmail.com',
+    license='MIT',
 )

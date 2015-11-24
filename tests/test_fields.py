ee4b12a5a787020882f3f69f72815705105a2d57
Conan Li <conanlics@gmail.com>
2015-11-24 00:14:13 -0400

    : writing some tests

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 from decimal import Decimal
 
 from django.test import TestCase
@@ -10,13 +11,25 @@ class DummyModel(models.Model):
     hstores = GosHStoreField(default={})
     second_gos = GosHStoreField(default={})
 
+    def calculate(self):
+        return 10 * 10
+
     @hstores.getter(key='batman', converter=Decimal)
     def gos_batman(self, save=True, reset=False):
-        return 100 * 100
+        return self.calculate()
 
 
 class GosHstoreFieldTestCase(TestCase):
 
-    def test_goshstorefield_no_args(self):
+    @patch.object(DummyModel, 'calculate')
+    def test_goshstorefield_save_false(self, mock):
+        '''If hstore field (in this case named "hstores") is empty, calling
+        gos_batman should call calculate().
+        '''
+        mock.return_value = 100 * 100
         instance = DummyModel()
-        print(instance.gos_batman())
+        self.assertFalse(instance.hstores)
+        self.assertEqual(instance.gos_batman(save=False),
+                         Decimal(instance.calculate()))
+        self.assertEqual(instance.hstores,
+                         {'batman': str(instance.calculate())})
